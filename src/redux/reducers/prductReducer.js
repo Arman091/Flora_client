128dbae567a26656ef3dfe728dc315108b5b09d7
Arman091 <rasoolarman7@gmail.com>
2023-08-04 16:54:56 +0530

    "update"

--- src/redux/reducers/prductReducer.js
+++ src/redux/reducers/prductReducer.js
@@ -1,6 +1,6 @@
 import * as actionTypes from "../constants/productConstant";
 
-//6. Ye yaha action type check krega aur ussi hisab se data return kr diya isne, aur store me chali gya sara data
+
 const getAllProductsReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case actionTypes.GET_ALL_PRODUCTS_SUCCESS:
