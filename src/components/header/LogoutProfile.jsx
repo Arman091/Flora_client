4ef58b1fc026aafdf86f7828f29ef1ea22766eae
arman091 <rasoolarman7@gmail.com>
2026-07-18 12:09:51 +0530

    bugfix/header logout button

--- src/components/header/LogoutProfile.jsx
+++ src/components/header/LogoutProfile.jsx
@@ -1,65 +1,62 @@
-import { Typography, Box, MenuItem, Menu, styled } from "@mui/material";
+import { Typography, Box, MenuItem, MenuList, Paper, Popper, styled } from "@mui/material";
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
 import { useAuth } from "../../context/AuthProvider";
-const Component = styled(Menu)`
+import { useClickOutside } from "../../shared/helpers";
+
+const Dropdown = styled(Paper)`
   margin-top: 5px;
+  z-index: 1500;
 `;
 
 const Signout = styled(Typography)`
   font-size: 14px;
   margin-right:12px;
-
 `;
 
 const SignoutIcon = styled(PowerSettingsNewIcon)`
   color: var(--color-text-primary);
-
 `;
 
 const LogoutProfile = ({ user }) => {
-  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
   const { logout } = useAuth();
+  const ref = useRef(null);
+
+  useClickOutside(ref, () => setAnchorEl(null));
 
   const handleClick = (event) => {
-    setOpen(event.currentTarget);
+    setAnchorEl(anchorEl ? null : event.currentTarget);
   };
 
   const handleClose = () => {
-    setOpen(false);
+    setAnchorEl(null);
   };
-
-  const handleLogout = () => {
-    logout();
-  };
-
 
   return (
-    <>
-      <Box onClick={handleClick}>
-        <Typography 
-          style={{ marginTop: 3, cursor:"pointer", color: "var(--color-text-primary)", marginRight: 18 }}>
-          {user?.firstName || user?.name }
-        </Typography>
-        <Component
-          id="basic-menu"
-          anchorEl={open}
-          open={open}
-          onClose={handleClose}
-        >
-          <MenuItem
-            onClick={() => {
-              handleClose();
-              handleLogout();
-            }}
-          >
-            <SignoutIcon  className="myicon"/>
-            <Signout>Sign Out</Signout>
-          </MenuItem>
-        </Component>
-      </Box>
-    </>
+    <Box ref={ref}>
+      <Typography 
+        onClick={handleClick}
+        style={{ marginTop: 3, cursor:"pointer", color: "var(--color-text-primary)", marginRight: 18 }}>
+        {user?.firstName || user?.name }
+      </Typography>
+      <Popper open={Boolean(anchorEl)} anchorEl={anchorEl} placement="bottom-start" sx={{ zIndex: 1500 }} disablePortal>
+        <Dropdown>
+          <MenuList>
+            <MenuItem
+              onClick={() => {
+                handleClose();
+                logout();
+              }}
+            >
+              <SignoutIcon  className="myicon"/>
+              <Signout>Sign Out</Signout>
+            </MenuItem>
+          </MenuList>
+        </Dropdown>
+      </Popper>
+    </Box>
   );
 };
 
